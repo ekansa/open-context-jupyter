722904ef589f578fb0593fb3f7022e31fc2ae14c
Eric Kansa <kansaeric@gmail.com>
2020-06-11 13:03:47 -0700

    Better OpenContextAPI caching, context handling

--- opencontext/api.py
+++ opencontext/api.py
@@ -8,6 +8,7 @@ import numpy as numpy
 import pandas as pd
 
 from datetime import date
+from slugify import slugify
 from time import sleep
 
 
@@ -17,6 +18,10 @@ class OpenContextAPI():
         
         See API documentation here: https://opencontext.org/about/services
     """
+
+    # The name of the directory for file caching JSON data from Open Context 
+    API_CACHE_DIR = 'oc-api-cache'
+
     RECS_PER_REQUEST = 200  # number of records to retrieve per request
     FLATTEN_ATTRIBUTES = True  # make sure attributes are single value, not lists
     RESPONSE_TYPE_LIST = ['metadata', 'uri-meta']
@@ -41,12 +46,42 @@ class OpenContextAPI():
 
     FACET_OPTIONS_KEYS = TEXT_FACET_OPTION_KEYS + NON_TEXT_OPTION_KEYS
 
+    VON_DEN_DRIESCH_PROP = 'oc-zoo-anatomical-meas---oc-zoo-von-den-driesch-bone-meas'
 
     def __init__(self):
         self.recs_per_request = self.RECS_PER_REQUEST
         self.sleep_time = self.SLEEP_TIME
         self.flatten_attributes = self.FLATTEN_ATTRIBUTES
         self.response_types = self.RESPONSE_TYPE_LIST
+
+        # The cache prefix is a prefix that defaults to a
+        # string representation of today's date.
+        self.cache_file_prefix = date.today().strftime('%Y-%m-%d')
+
+
+    def set_cache_file_prefix(self, text_for_prefix):
+        """Makes a 'slug-ified' cache file prefix"""
+        self.cache_file_prefix = slugify(text_for_prefix)
+
+
+    def _modify_get_params_by_url_check(self, url, params):
+        """Makes an extra params dict for parameters NOT already in a URL"""
+        # Add the parameters that are not actually already in the
+        # url.
+        if not params:
+            return {}
+
+        extra_params = {
+            k:v for k,v in params.items() if (k + '=') not in url
+        }
+        if params.get('prop') and not extra_params.get('prop'):
+            # The param 'prop' is special, since we can have more than
+            # one of these in a url.
+            if ('prop=' + params['prop']) not in url:
+                # This particular prop and value is not already in the
+                # url, so it's OK to add to the extra_params dict.
+                extra_params['prop'] = params['prop']
+        return extra_params
 
 
     def _make_url_cache_file_name(self, url, extra_params={}, extension='.json'):
@@ -58,6 +93,10 @@ class OpenContextAPI():
 
         extra_suffix = ''
         if extra_params:
+            extra_params = self._modify_get_params_by_url_check(
+                url,
+                extra_params
+            )
             # We have some extra paramaters, add a suffix to the URL
             # by dumping these as a string. This is not meant to make
             # real URL, just to make a cache-key that captures all
@@ -68,7 +107,7 @@ class OpenContextAPI():
         hash_obj.update((url + extra_suffix).encode('utf-8'))
         hash_url = hash_obj.hexdigest()
         cache_file_name = (
-            date.today().strftime('%Y-%m-%d')
+            self.cache_file_prefix
             + '-'
             + hash_url
             + extension
@@ -76,22 +115,27 @@ class OpenContextAPI():
         return cache_file_name
     
 
-    def clean_api_cache(self, keep_today=True):
+    def clear_api_cache(self, keep_prefix=True):
         """Cleans old data from the API cache"""
         repo_path = os.path.dirname(os.path.abspath(os.getcwd()))
-        cache_path = os.path.join(
-            repo_path, 'oc-api-cache'
-        )
 
+        cache_dir = os.path.join(
+            repo_path, self.API_CACHE_DIR
+        )
+        if not os.path.exists(cache_dir):
+            # No cache directory exists, so nothing to erase.
+            return None
+        
         # Prefix for filenames to keep (from today)
         keep_prefix = date.today().strftime('%Y-%m-%d') + '-'
-        for f in os.listdir(cache_path):
-            file_path = os.path.join(cache_path, f)
+        for f in os.listdir(cache_dir):
+            file_path = os.path.join(cache_dir, f)
             if not os.path.isfile(file_path):
                 # Not a file, so skip
                 continue
-            if keep_today and f.startswith(keep_prefix):
-                # This file is from today, so keep it.
+            if keep_prefix and f.startswith(self.cache_file_prefix):
+                # We skip because we're keeping files that start with
+                # the current self.cache_file_prefix
                 continue
             os.remove(file_path)
 
@@ -100,7 +144,7 @@ class OpenContextAPI():
         """Returns an object parsed from cached json"""
         repo_path = os.path.dirname(os.path.abspath(os.getcwd()))
         path_file = os.path.join(
-            repo_path, 'oc-api-cache', cache_file_name
+            repo_path, self.API_CACHE_DIR, cache_file_name
         )
         try:
             obj_from_json = json.load(
@@ -114,8 +158,15 @@ class OpenContextAPI():
     def _cache_json(self, cache_file_name, obj_to_json):
         """Caches an object as json to a cache_file_name"""
         repo_path = os.path.dirname(os.path.abspath(os.getcwd()))
+        cache_dir = os.path.join(
+            repo_path, self.API_CACHE_DIR
+        )
+        if not os.path.exists(cache_dir):
+            # Make sure we actually have the cache directory.
+            os.makedirs(cache_dir)
+    
         path_file = os.path.join(
-            repo_path, 'oc-api-cache', cache_file_name
+            cache_dir, cache_file_name
         )
         json_output = json.dumps(
             obj_to_json,
@@ -145,6 +196,11 @@ class OpenContextAPI():
             'accept': 'application/json'
         }
 
+        extra_params = self._modify_get_params_by_url_check(
+            url,
+            extra_params
+        )
+
         try:
             sleep(self.sleep_time)  # pause to not overwhelm the API
             r = requests.get(url, params=extra_params, headers=headers)
@@ -162,7 +218,11 @@ class OpenContextAPI():
         return obj_from_oc
 
     
-    def get_standard_attributes(self, url):
+    def get_standard_attributes(
+        self, 
+        url, 
+        add_von_den_driesch_bone_measures=False
+    ):
         """Gets the 'standard' attributes from a search URL
         """
         # -------------------------------------------------------------
@@ -170,7 +230,15 @@ class OpenContextAPI():
         # meaning attributes that are with data from multiple projects.
         # These attributes are typically defined by
         # -------------------------------------------------------------
-        json_data = self.get_cache_url(url)
+        extra_params = {}
+        if add_von_den_driesch_bone_measures:
+            # Standard Von Den Driesch bone measurement attributes are 
+            # a little buried in Open Context's API. If this argument 
+            # is True, we add a parameter to the GET request to make
+            # sure that we have it.
+            extra_params['prop'] = self.VON_DEN_DRIESCH_PROP  
+
+        json_data = self.get_cache_url(url, extra_params=extra_params)
         
         if not json_data:
             # Somthing went wrong, so skip out.
@@ -314,7 +382,8 @@ class OpenContextAPI():
     def get_paged_json_records(self, 
         url, 
         attribute_slugs, 
-        do_paging=True
+        do_paging=True,
+        split_contexts=True
     ):
         """Gets records data from a URL, recursively get next page
         """
@@ -328,19 +397,25 @@ class OpenContextAPI():
         if self.flatten_attributes:
             params['flatten-attributes'] = 1
         
-        # Add the parameters that are not actually already in the
-        # url.
-        extra_params = {
-            k:v for k,v in params.items() if (k + '=') not in url
-        }
-        
-        json_data = self.get_cache_url(url, extra_params=extra_params)
+        json_data = self.get_cache_url(url, extra_params=params)
         
         if not json_data:
             # Somthing went wrong, so skip out.
             return None
 
-        records = json_data.get('oc-api:has-results', [])
+        raw_records = json_data.get('oc-api:has-results', [])
+        records = []
+        for rec in raw_records:
+            new_rec = {}
+            for key, val in rec.items():
+                if key != 'context label':
+                    new_rec[key] = val
+                else:
+                    contexts = val.split('/')
+                    for i, context in enumerate(contexts, 1):
+                        new_rec['Context ({})'.format(i)] = context
+            records.append(new_rec)
+                
         next_url = json_data.get('next')
 
         if do_paging and next_url:
